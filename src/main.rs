3fc2506ea2c5651d263fe2ff123b7561535424ab
Hitoshi Amano <seijro@gmail.com>
2024-04-06 14:46:12 +0900

    first step

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use anyhow::Result;
+use actix_web::{App, HttpServer};
+use actix_files::Files;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/", "./frontend/dist").index_file("index.html").prefer_utf8(true))
+    }).bind(("localhost", 3000))?.run().await?;
+    Ok(())
+}
+
